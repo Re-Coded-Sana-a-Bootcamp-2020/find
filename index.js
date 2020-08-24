9c5a4028bcb5a7e1065b926ac47b4f76c96a7f33
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-08-24 13:13:18 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const nums = [-3, -2, 0, 4];
+
+let firstPositive;
+
+// A sample solution using for loops might look like this.
+// for (let n of nums) {
+//   if (n > 0) {
+//     firstPositive = n;
+//     break;
+//   }
+// }
+
+// Create the same result as above with find.
